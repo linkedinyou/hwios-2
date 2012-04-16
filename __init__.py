f3c547337ed778dd0821b1961a1cba008ecd93d3
OS-Networks <info@os-networks.net>
2012-04-16 22:20:32 +0200

    bla

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from twisted.python import log
+
+def dws_started():
+    log.msg('Starting django-ws project')
--- urls.py
+++ urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
-from dws.core.urls import urlpatterns as urls, wurlpatterns as wurls
+from dws.core.urls import urls, wurls
 from django.conf import settings
 from dws.core.views import BootstrapView
 
 urlpatterns = urls
-urlpatterns += patterns('',
+urlpatterns.extend(patterns('',
         url(r'^', BootstrapView.as_view(template_name="index.html")),
-)
+))
 
-wurlpatterns = [].extend(wurls)
+
+wurlpatterns = []
+wurlpatterns.extend(wurls)
