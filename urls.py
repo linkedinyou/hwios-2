0985a6393f1eb1144380f268939a6f3f0a483c60
Jeroen van Veen <gww@localhost.localdomain>
2012-08-07 18:32:04 +0200

    bla

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
-from dws.core.urls import urls, wurls
+from dws.core.urls import wurls
 from django.conf import settings
 from dws.core.views import BootstrapView
 
-urlpatterns = urls
-urlpatterns.extend(patterns('',
+
+urlpatterns = (patterns('',
         (r'^ckeditor/', include('ckeditor.urls')),
-        (r'^misc/', include('dws.misc.urls')),
+        (r'^', include('dws.core.urls')),
 
         url(r'^', BootstrapView.as_view(template_name="index.html")),
         
